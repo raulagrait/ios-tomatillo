2b7a2e26d19a7f32e0b78b644856e1a961480763
Raul Agrait <raul.agrait@rd.io>
2015-04-16 22:47:19 -0700

    CodePath: Tomatillo: Add tab bar to choose between box office and DVD rentals

--- Tomatillo/MoviesViewController.swift
+++ Tomatillo/MoviesViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITabBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var errorView: UIView!
-    
+    @IBOutlet weak var tabBar: UITabBar!
     var movies: [NSDictionary]?
     var refreshControl: UIRefreshControl!
+    var loadBoxOffice: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +23,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // I tried for way too long to try to do this in IB
         view.bringSubviewToFront(self.errorView)
         
-        tableView.hidden = true
-        SVProgressHUD.show()
-        
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tabBar.delegate = self
+        tabBar.selectedItem = tabBar.items![0] as? UITabBarItem
         
         refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
         tableView.insertSubview(refreshControl, atIndex: 0)
+        
+        load()
+    }
+    
+    func load() {
+        tableView.hidden = true
+        SVProgressHUD.show()
         
         loadMovies({
             self.tableView.hidden = false
@@ -38,8 +46,21 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         })
     }
     
+    func reload() {
+        loadMovies({
+            self.refreshControl.endRefreshing()
+        })
+    }
+    
+    func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem!) {
+        loadBoxOffice = (item.tag == 0)
+        load()
+    }
+    
     func loadMovies(callback: () -> Void) {
-        let url = NSURL(string: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=dagqdghwaq3e3mxyrp7kmmj5&limit=20&country=US")!
+        var type = loadBoxOffice ? "movies/box_office" : "dvds/top_rentals"
+        let url = NSURL(string: "http://api.rottentomatoes.com/api/public/v1.0/lists/\(type).json?apikey=dagqdghwaq3e3mxyrp7kmmj5&limit=20&country=US")!
+
         var timeoutInterval = 60.0 // 0.1
         let request = NSURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: timeoutInterval)
         
@@ -47,17 +68,15 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             if (error != nil) {
                 self.errorView.hidden = false
             } else {
-                let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionaryxw
+                let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
                 self.errorView.hidden = true
                 if let json = json {
                     self.movies = json["movies"] as? [NSDictionary]
                     self.tableView.reloadData()
-                    
                 }
             }
             callback()
         }
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -90,11 +109,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func onRefresh() {
-        println("we got refreshed")
-        
-        loadMovies({
-            self.refreshControl.endRefreshing()
-        })
+        reload()
     }
 
     // MARK: - Navigation
